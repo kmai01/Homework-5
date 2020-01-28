bf429c019de5c293756762dddbc76065aa1042ee
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-01-28 16:19:18 -0600

    3rd update

--- script.js
+++ script.js
@@ -1,5 +1,44 @@
-$(document).ready(function () {
-    
 
+
+
+var nineAM = $('#lead');
+var nineAMM = moment().format();
+nineAM.textContent=nineAMM;
+console.log(nineAMM);
+
+
+
+
+
+
+event =localStorage.getItem("event");
+
+console.log(event)
+$("#form1").text(event);
+
+
+
+$('#saveButton1').click(function () {
+
+    storeevent();
 
 });
+
+
+
+
+// set the item in localStorage
+function storeevent() {
+
+    var content = $('#form1').val();
+
+    localStorage.setItem("event", content);
+
+    console.log(content);
+
+}
+
+
+
+ 
+
