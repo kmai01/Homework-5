47a777bf2e9d16e0dd2aa79409cd008f19adbe97
Khanh Mai <57655821+kmai01@users.noreply.github.com>
2020-01-30 02:54:39 -0600

    add save storage

--- script.js
+++ script.js
@@ -1,36 +1,137 @@
-var timelabel=["9AM","10AM","11AM","12PM","1PM","2PM","3PM","4PM","5PM"]
+// time label , input form , button in array
+
+var timelabel = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+var inputform = [];
+var savebutton = [];
+
+
+
 
 // loop create time slot with bootstrap class
 
-for ( var i=0 ; i < 9; i++) { 
+for (var i = 0; i < 9; i++) {
 
-var inputgroupmb3Div= $("<div>")
-inputgroupmb3Div.addClass("input-group mb-3")
-$("#Entry-Container").append(inputgroupmb3Div);
+    var inputgroupmb3Div = $("<div>")
+    inputgroupmb3Div.addClass("input-group mb-3")
+    $("#Entry-Container").append(inputgroupmb3Div);
 
-var inputgroupprependDiv= $("<div>")
-inputgroupprependDiv.addClass("input-group-prepend")
-inputgroupmb3Div.append(inputgroupprependDiv);
+    var inputgroupprependDiv = $("<div>")
+    inputgroupprependDiv.addClass("input-group-prepend")
+    inputgroupmb3Div.append(inputgroupprependDiv);
 
-var inputgrouppretextDiv= $("<span>")
-inputgrouppretextDiv.addClass("input-group-text")
-inputgrouppretextDiv.text(timelabel[i])
-inputgroupprependDiv.append(inputgrouppretextDiv);
+    var inputgrouppretextDiv = $("<span>")
+    inputgrouppretextDiv.addClass("input-group-text")
+    inputgrouppretextDiv.text(timelabel[i])
+    inputgroupprependDiv.append(inputgrouppretextDiv);
 
-var inputform=$('<input />', {type:'text', class:'form-control'})
-inputgroupmb3Div.append(inputform);
+    inputform[i] = $('<input />', { type: 'text', class: 'form-control' })
+    inputgroupmb3Div.append(inputform[i]);
 
-var inputgroupappend=$("<div>")
-inputgroupappend.addClass("input-group-append")
-inputgroupmb3Div.append(inputgroupappend);
+    var inputgroupappend = $("<div>")
+    inputgroupappend.addClass("input-group-append")
+    inputgroupmb3Div.append(inputgroupappend);
 
-var savebutton=$("<button>")
-savebutton.addClass("btn btn-outline-secondary")
-savebutton.text("Save")
-inputgroupappend.append(savebutton)
+    savebutton[i] = $("<button>")
+    savebutton[i].addClass("btn btn-outline-secondary")
+    savebutton[i].text("Save")
+    inputgroupappend.append(savebutton[i])
 
 }
 
+// Re-populate localstorage back to form after refresh
+
+var event0 = localStorage.getItem("event0");
+inputform[0].val(event0);
+var event1 = localStorage.getItem("event1");
+inputform[1].val(event1);
+var event2 = localStorage.getItem("event2");
+inputform[2].val(event2);
+var event3 = localStorage.getItem("event3");
+inputform[3].val(event3);
+var event4 = localStorage.getItem("event4");
+inputform[4].val(event4);
+var event5 = localStorage.getItem("event5");
+inputform[5].val(event5);
+var event6 = localStorage.getItem("event6");
+inputform[6].val(event6);
+var event7 = localStorage.getItem("event7");
+inputform[7].val(event7);
+var event8 = localStorage.getItem("event8");
+inputform[8].val(event8);
+
+
+
+savebutton[0].click(function () {
+
+    var content = inputform[0].val();
+    localStorage.setItem("event0", content);
+    console.log(content);
+
+});
+
+savebutton[1].click(function () {
+
+    var content = inputform[1].val();
+    localStorage.setItem("event1", content);
+    console.log(content);
+
+});
+
+savebutton[2].click(function () {
+
+    var content = inputform[2].val();
+    localStorage.setItem("event2", content);
+    console.log(content);
+
+});
+
+savebutton[3].click(function () {
+
+    var content = inputform[3].val();
+    localStorage.setItem("event3", content);
+    console.log(content);
+
+});
+
+savebutton[4].click(function () {
+
+    var content = inputform[4].val();
+    localStorage.setItem("event4", content);
+    console.log(content);
+
+});
+
+savebutton[5].click(function () {
+
+    var content = inputform[5].val();
+    localStorage.setItem("event5", content);
+    console.log(content);
+
+});
+
+savebutton[6].click(function () {
+
+    var content = inputform[6].val();
+    localStorage.setItem("event6", content);
+    console.log(content);
+
+});
+
+savebutton[7].click(function () {
+
+    var content = inputform[7].val();
+    localStorage.setItem("event7", content);
+    console.log(content);
+
+});
+
+savebutton[8].click(function () {
+
+    var content = inputform[8].val();
+    localStorage.setItem("event8", content);
+    console.log(content);   
+
+});
 
 
 
@@ -39,7 +140,7 @@ inputgroupappend.append(savebutton)
 
 
 
-var current= $("time");
+var current = $("time");
 var time = moment().format('MMMM Do YYYY, h:mm:ss a');
 console.log(time)
 current.text(time);
@@ -50,49 +151,29 @@ var eightAM = 8
 console.log(eightAM);
 
 
- var currentDate = new Date()
- 
-var momentDate = moment(currentDate).format('H'); 
-console.log(momentDate) 
+var currentDate = new Date()
 
-
-
-// Re-populate localstorage back to form after refresh
-
-event =localStorage.getItem("event");
-
-console.log(event)
-$("#form1").val(event);
-
-
-// button to save text entry
-
-$('#saveButton1').click(function () {
-
-    storeevent();
-
-});
+var momentDate = moment(currentDate).format('H');
+console.log(momentDate)
 
 
 
 
-// set the item in localStorage
-function storeevent() {
 
-    var content = $('#form1').val();
 
-    localStorage.setItem("event", content);
 
-    console.log(content);
+
+
+
+
+
+
+
+if (eightAM < momentDate) {
+
+    $('#form1').css("background-color", "blue");
+
 
 }
 
-
-if ( eightAM < momentDate ) {
-
-    $('#form1').css("background-color",  "blue");
-    
-
-}
- 
 
